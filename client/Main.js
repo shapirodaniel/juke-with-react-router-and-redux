a50c8c032548e831153b7039d2f5a9cbfa3eb671
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-20 07:06:14 -0500

    add: client

--- client/Main.js
+++ client/Main.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import Sidebar from './Sidebar';
+import SingleAlbum from './SingleAlbum';
+import AlbumsList from './AlbumsList';
+import Player from './Player';
 
 export default class Main extends React.Component {
-  render () {
-    return (
-      <div id='main' className='row container'>
-        {/* The music starts here! */}
-      </div>
-    )
-  }
+	constructor() {
+		super();
+		this.state = {
+			albums: [],
+			selectedAlbum: {},
+		};
+		this.pickAlbum = this.pickAlbum.bind(this);
+		this.deselectAlbum = this.deselectAlbum.bind(this);
+
+		console.log('Main constructor done!');
+	}
+
+	async componentDidMount() {
+		console.log('Main componentDidMount executing!');
+		const { data } = await axios.get('/api/albums');
+		this.setState({
+			albums: data,
+		});
+	}
+
+	// Option 1: Original solution
+	// pickAlbum (albumId) {
+	//   return async () => {
+	//     const {data} = await axios.get(`/api/albums/${albumId}`)
+	//     this.setState({
+	//       selectedAlbum: data
+	//     })
+	//   }
+	// }
+
+	// Option 2: Morning Review solution
+	async pickAlbum(albumId) {
+		const { data } = await axios.get(`/api/albums/${albumId}`);
+		this.setState({
+			selectedAlbum: data,
+		});
+	}
+
+	deselectAlbum() {
+		this.setState({
+			selectedAlbum: {},
+		});
+	}
+
+	componentDidUpdate() {
+		console.log('Main updated (componentDidUpdate)!');
+	}
+
+	render() {
+		const { next, prev, toggle, toggleOne, currentSong, isPlaying } =
+			this.props;
+		console.log('Main rendered!');
+
+		return (
+			<div id='main' className='row container'>
+				<Sidebar deselectAlbum={this.deselectAlbum} />
+				<div className='container'>
+					{this.state.selectedAlbum.id ? (
+						<SingleAlbum
+							album={this.state.selectedAlbum}
+							toggleOne={toggleOne}
+							isPlaying={isPlaying}
+							currentSong={currentSong}
+						/>
+					) : (
+						<AlbumsList albums={this.state.albums} pickAlbum={this.pickAlbum} />
+					)}
+				</div>
+				<Player prev={prev} next={next} toggle={toggle} isPlaying={isPlaying} />
+			</div>
+		);
+	}
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Main from './Main'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Audio from './Audio';
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Audio />, document.getElementById('app'));
