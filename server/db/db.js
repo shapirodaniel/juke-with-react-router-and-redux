e64c722247f514ebd23b4c1f49ade5460c7c357f
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-20 07:01:34 -0500

    add: server

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,9 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
-  logging: false
-})
+const Sequelize = require('sequelize');
+const db = new Sequelize(
+	process.env.DATABASE_URL || 'postgres://localhost:5432/juke',
+	{
+		logging: false,
+	}
+);
 
-module.exports = db
+module.exports = db;
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,20 @@
-const db = require('./db')
-// require each of your models here...
+const db = require('./db');
+const Album = require('./album');
+const Artist = require('./artist');
+const Song = require('./song');
 
-// ...and give them some nice associations here!
+Song.belongsTo(Album);
+Album.hasMany(Song);
+
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
+
+Album.belongsTo(Artist);
+Artist.hasMany(Album);
 
 module.exports = {
-  db,
-  // Include your models in your module.exports as well!
-  // The seed file expects to find them there!
-}
+	db,
+	Album,
+	Artist,
+	Song,
+};
