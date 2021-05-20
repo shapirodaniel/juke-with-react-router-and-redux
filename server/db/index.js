92d59bcd238b3dde5ed059b197074df06644b316
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-20 07:12:09 -0500

    fix: merge conflict

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,4 @@
 const db = require('./db');
-<<<<<<< HEAD
 const Album = require('./album');
 const Artist = require('./artist');
 const Song = require('./song');
@@ -9,35 +8,13 @@ Album.hasMany(Song);
 
 Song.belongsTo(Artist);
 Artist.hasMany(Song);
-=======
-// require each of your models here...
-const Album = require('./Album');
-const Artist = require('./Artist');
-const Song = require('./Song');
-
-// ...and give them some nice associations here!
-Album.hasMany(Song);
-Song.belongsTo(Album);
-
-Artist.hasMany(Song);
-Song.belongsTo(Artist);
->>>>>>> ea9b6a57f4a73ed972d96ffe25e19af14eab7897
 
 Album.belongsTo(Artist);
 Artist.hasMany(Album);
 
 module.exports = {
-<<<<<<< HEAD
 	db,
 	Album,
 	Artist,
 	Song,
-=======
-  db,
-  Album,
-  Artist,
-  Song,
-  // Include your models in your module.exports as well!
-  // The seed file expects to find them there!
->>>>>>> ea9b6a57f4a73ed972d96ffe25e19af14eab7897
 };
