e948ee1f1c2cb9ae7a9accac1118c188dcf06ea3
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-21 08:13:42 -0500

    refactor: all albums with hooks

--- client/components/AllAlbums.js
+++ client/components/AllAlbums.js
@@ -1,35 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAlbums } from '../redux/albums';
 import { AlbumCard } from './';
 
-class AllAlbums extends React.Component {
-	componentDidMount() {
-		this.props.loadAlbums();
-	}
+const AllAlbums = () => {
+	const dispatch = useDispatch();
 
-	render() {
-		const { albums } = this.props || [];
+	useEffect(() => {
+		dispatch(fetchAlbums());
+	}, []);
 
-		return (
-			<div id='albums' className='row wrap'>
-				{albums.map(album => (
-					<Link key={album.id} to={`/albums/${album.id}`}>
-						<AlbumCard album={album} />
-					</Link>
-				))}
-			</div>
-		);
-	}
-}
+	const albums = useSelector(state => state.albums);
 
-const mapState = state => ({
-	albums: state.albums,
-});
+	return (
+		<div id='albums' className='row wrap'>
+			{albums.map(album => (
+				<Link key={album.id} to={`/albums/${album.id}`}>
+					<AlbumCard album={album} />
+				</Link>
+			))}
+		</div>
+	);
+};
 
-const mapDispatch = dispatch => ({
-	loadAlbums: () => dispatch(fetchAlbums()),
-});
-
-export default connect(mapState, mapDispatch)(AllAlbums);
+export default AllAlbums;
