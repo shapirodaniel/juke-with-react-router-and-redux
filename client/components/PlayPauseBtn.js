a66a07b5e3b0c3c81191157c5ceb82776e2354e7
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-20 21:28:07 -0500

    refactor: play pause btn to use store logic for icon

--- client/components/PlayPauseBtn.js
+++ client/components/PlayPauseBtn.js
@@ -13,46 +13,32 @@ class PlayPauseBtn extends React.Component {
 		this.state = {
 			btnIcon: playIcon,
 		};
-		this.toggleIcon = this.toggleIcon.bind(this);
-	}
-
-	componentDidUpdate(prevProps) {
-		// first if-check keeps us from re-toggling the clicked btn
-		if (!prevProps.currentSong.id) return;
-		// second if-check sets icons for the other btns after clicked btn is toggled
-		if (prevProps.currentSong.id !== this.props.currentSong.id) {
-			const { currentSong, song } = this.props;
-			this.toggleIcon(currentSong, song);
-		}
-	}
-
-	toggleIcon(currentSong, song) {
-		if (!currentSong.id || currentSong.id === song.id) {
-			this.setState(state => ({
-				btnIcon: state.btnIcon === playIcon ? pauseIcon : playIcon,
-			}));
-		} else {
-			this.setState({ btnIcon: playIcon });
-		}
 	}
 
 	render() {
-		const { song, currentSong, setCurrentSong, setPaused } = this.props;
-		const { toggleIcon } = this;
+		const { song, currentSong, setCurrentSong, isPaused, setPaused } =
+			this.props;
 
 		const handleClick = () => {
 			setCurrentSong(song);
 			const status = handlePlayerBtnClick(song.audioUrl);
-			toggleIcon(currentSong, song);
 			setPaused(status);
 		};
 
-		return <i className={this.state.btnIcon} onClick={handleClick} />;
+		return (
+			<i
+				className={
+					song.id === currentSong.id && !isPaused ? pauseIcon : playIcon
+				}
+				onClick={handleClick}
+			/>
+		);
 	}
 }
 
 const mapState = state => ({
 	currentSong: state.currentSong,
+	isPaused: state.isPaused,
 });
 
 const mapDispatch = dispatch => ({
