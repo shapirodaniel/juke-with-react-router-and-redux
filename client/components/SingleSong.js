36ad1fb07769a996e836eb1ed37a26861b621d66
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-27 06:30:08 -0500

    refactor: single song to class component

--- client/components/SingleSong.js
+++ client/components/SingleSong.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import PlayPauseBtn from './PlayPauseBtn';
 
-const SingleSong = ({ trackNumber, song }) => {
-	const currentSong = useSelector(state => state.currentSong);
-	const isPaused = useSelector(state => state.audio.isPaused);
+class SingleSong extends React.Component {
+	render() {
+		const { song, currentSong, isPaused, trackNumber } = this.props;
+		const { name, artist, genre } = song;
+		const isSelected = !currentSong.id || currentSong.id === song.id;
+		const isActiveSong = isSelected && !isPaused;
 
-	const { name, artist, genre } = song;
+		return (
+			<tr className={isActiveSong ? 'active' : ''}>
+				<td>
+					{/* song prop used to compare against currentSong and conditionally render single song play/pause btn icons */}
+					<PlayPauseBtn song={song} />
+				</td>
+				<td>{trackNumber}</td>
+				<td>{name}</td>
+				<td>{artist.name}</td>
+				<td>{genre}</td>
+			</tr>
+		);
+	}
+}
 
-	const isSelected = !currentSong.id || currentSong.id === song.id;
+const mapState = state => ({
+	currentSong: state.currentSong,
+	isPaused: state.isPaused,
+});
 
-	// pass down status of audio somehow here?
-	const isActiveSong = isSelected && !isPaused;
-
-	return (
-		<tr className={isActiveSong ? 'active' : ''}>
-			<td>
-				{/* song prop used to compare against currentSong and conditionally render single song play/pause btn icons */}
-				<PlayPauseBtn song={song} />
-			</td>
-			<td>{trackNumber}</td>
-			<td>{name}</td>
-			<td>{artist.name}</td>
-			<td>{genre}</td>
-		</tr>
-	);
-};
-
-export default SingleSong;
+export default connect(mapState)(SingleSong);
