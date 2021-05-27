98858f45192b000ad041eca6955b846a175121a5
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-27 06:27:50 -0500

    refactor: single album to class component

--- client/components/SingleAlbum.js
+++ client/components/SingleAlbum.js
@@ -1,31 +1,37 @@
-import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { fetchCurrentAlbum } from '../redux/currentAlbum';
 import { AlbumCard, AllSongs } from './';
 
-const SingleAlbum = () => {
-	const { id } = useParams();
+class SingleAlbum extends React.Component {
+	componentDidMount() {
+		this.props.fetchCurrentAlbum(this.props.match.params.id);
+	}
 
-	const dispatch = useDispatch();
+	render() {
+		const { currentAlbum } = this.props;
 
-	useEffect(() => {
-		dispatch(fetchCurrentAlbum(id));
-	}, []);
-
-	const currentAlbum = useSelector(state => state.currentAlbum);
-
-	return (
-		<div id='single-album' className='column'>
-			<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-				<AlbumCard album={currentAlbum} />
-				<Link to='/'>
-					<div className='backBtn'>{'<< Back To All Albums'}</div>
-				</Link>
+		return (
+			<div id='single-album' className='column'>
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<AlbumCard album={currentAlbum} />
+					<Link to='/'>
+						<div className='backBtn'>{'<< Back To All Albums'}</div>
+					</Link>
+				</div>
+				<AllSongs songs={currentAlbum.songs} />
 			</div>
-			<AllSongs songs={currentAlbum.songs} />
-		</div>
-	);
-};
+		);
+	}
+}
 
-export default SingleAlbum;
+const mapState = state => ({
+	currentAlbum: state.currentAlbum,
+});
+
+const mapDispatch = dispatch => ({
+	fetchCurrentAlbum: id => dispatch(fetchCurrentAlbum(id)),
+});
+
+export default connect(mapState, mapDispatch)(SingleAlbum);
